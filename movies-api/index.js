92095eb0e3fdec45b4778df69777f1f4da368b0c
lauradhg18 <lauradhg18@uma.es>
2023-12-03 15:45:42 +0100

    Added Pagination

--- movies-api/index.js
+++ movies-api/index.js
@@ -14,6 +14,7 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/movies',authenticate,  moviesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
